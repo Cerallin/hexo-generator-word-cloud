7857de67ae54a359d2dab0c8b67dad140534ac4a
Cerallin <cerallin@cerallin.top>
2022-04-23 13:14:20 +0800

    opt: Add more options
    Signed-off-by: Cerallin <cerallin@cerallin.top>

--- ext/word_cloud.py
+++ ext/word_cloud.py
@@ -1,11 +1,45 @@
+from getopt import getopt
 import json
 import sys
-from wordcloud import WordCloud
+from wordcloud import ImageColorGenerator, WordCloud
+import numpy as np
+from PIL import Image
+import os
 
-print(WordCloud(
-    width=800,
-    height=480,
-    max_font_size=64,
+min_font_size=4
+
+opts, args = getopt(sys.argv[1:], 'm:c:a:b:',
+                    ['mask=',
+                     'color=',
+                     'max-font-size=',
+                     'min-font-size='])
+
+for opt_name, opt_value in opts:
+    if opt_name in ('-m', '--mask'):
+        mask_image = np.array(Image.open(os.path.join(os.getcwd(), opt_value)))
+    if opt_name in ('-c', '--color'):
+        mask_color = ImageColorGenerator(
+            np.array(Image.open(os.path.join(os.getcwd(), opt_value))))
+    if opt_name in ('-a', '--max-font-size'):
+        size = opt_value
+        max_font_size = int(size)
+    if opt_name in ('-b', '--min-font-size'):
+        size = opt_value
+        min_font_size = int(size)
+
+wc = WordCloud(
+    mask=mask_image,
+    width=400,
+    height=240,
     mode="RGBA",
-    background_color=None
-).generate_from_frequencies(json.loads(''.join(sys.stdin.readlines()))).to_svg())
+    background_color=None,
+    min_font_size=min_font_size,
+    max_font_size=max_font_size,
+)
+
+wc.generate_from_frequencies(json.loads(''.join(sys.stdin.readlines())))
+
+if mask_color:
+    wc.recolor(color_func=mask_color)
+
+print(wc.to_svg())
--- readme.md
+++ readme.md
@@ -12,8 +12,27 @@ Python 3 and [word_cloud](https://github.com/amueller/word_cloud) are required.
 npm install hexo-generator-word-cloud
 ```
 
+## Options
+
+```yml
+word_cloud:
+  python_path: python3
+  output_path: word-cloud.svg
+  word_num: 100
+
+  # Generation options
+  mask_image: source/_jieba/mask.png
+  mask_image_color: source/_jieba/mask-color.png
+  max_font_size: 80
+  min_font_size: 4
+
+  # User dicts
+  user_dict: source/_jieba/user_dict.utf-8
+  stop_word_dict: source/_jieba/stop_word_dict.utf-8
+```
 
 ## TODOs
 
-- [ ] specify mask image
+- [x] specify mask image
+- [x] specify stop words
 - [ ] more options for word_cloud
